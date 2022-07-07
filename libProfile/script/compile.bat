95051f9f55f676e236ab518341019c6b8486c0f4
chengzhouliang <chengzhouliang@tenorshare.cn>
2022-07-07 17:38:08 +0800

    调整libProfile文件结构

---
+++ libProfile/script/compile.bat
@@ -0,0 +1,17 @@
+echo "set global path"
+set LIBPROFILE_WORKSPACE=%~dp0\..
+set LIBPROFILE_SRC=%LIBPROFILE_WORKSPACE%\src
+set LIBPROFILE_SCRIPT=%LIBPROFILE_WORKSPACE%\script
+set LIBPROFILE_BUILD=%LIBPROFILE_WORKSPACE%\build
+
+echo "choice package way"
+cd %LIBPROFILE_SRC% 
+if "%1"=="" (
+	echo "Packaging to a folder"
+) else (
+	echo "Packaging to a file"
+) 
+
+echo "packaging ..."
+pyinstaller %1 --clean -y insExtrator.py --distpath  %LIBPROFILE_BUILD%\bin --workpath %LIBPROFILE_BUILD%\obj
+cd %LIBPROFILE_SCRIPT%
